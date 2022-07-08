3f142b49a88cd93b4186c9e3dc36a746c7c60041
Stephen Hung <shthftwyes@gmail.com>
2022-07-09 00:51:47 +0800

    Added todo

--- src/components/TreeMap.tsx
+++ src/components/TreeMap.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 function TreeMap(props: any) {
-  console.log(props);
+  // TODO: implement packing algorithm
+  // https://cgi.csc.liv.ac.uk/~epa/surveyhtml.html
   return (
     <div style={{ width: 800, height: 800, backgroundColor: "white", color: "black" }}>
       Data: {props.data}
