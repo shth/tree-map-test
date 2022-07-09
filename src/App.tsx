cdedf158d9ffb2ef97e2662aa45160f104a64a3a
Stephen Hung <shthftwyes@gmail.com>
2022-07-09 11:15:10 +0800

    Finished input fields

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,46 @@ import "./App.css";
 import TreeMap from "./components/TreeMap";
 
 function App() {
-  const [rows, setRows] = useState("3");
-  const [jsonData, setJsonData] = useState("{}");
+  const [rows, setRows] = useState(3);
+  const [jsonData, setJsonData] = useState([]);
 
   return (
     <div className="App">
       <div className="tree-map-container">
         <div>
           <div>
-            <label>Data:</label>
-          </div>
-          <div>
-            <textarea onChange={(e) => setJsonData(e.target.value)}></textarea>
-          </div>
-          <div>
             <label>Row Number:</label>
           </div>
           <div>
-            <input type="number" onChange={(e) => setRows(e.target.value)}></input>
+            <input
+              type="number"
+              onChange={(e) => {
+                const result = parseInt(e.target.value);
+
+                if (isNaN(result)) {
+                  return;
+                }
+
+                return setRows(result);
+              }}
+            ></input>
+          </div>
+          <div>
+            <label>Data:</label>
+          </div>
+          <div>
+            <textarea
+              onChange={(e) => {
+                try {
+                  const json = JSON.parse(e.target.value);
+                  setJsonData(json);
+                } catch (e) {
+                  console.log(e);
+                }
+
+                return;
+              }}
+            ></textarea>
           </div>
         </div>
         <div>
